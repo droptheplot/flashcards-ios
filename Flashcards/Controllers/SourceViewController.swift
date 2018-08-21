0b6b644330e4e2e5bf645a0199ae41cae18cf102
Sergei Novikov <novikov359@gmail.com>
2018-08-21 14:07:11 +0300

    Add cards to source

--- Flashcards/Controllers/SourceViewController.swift
+++ Flashcards/Controllers/SourceViewController.swift
@@ -9,20 +9,42 @@
 import UIKit
 
 class SourceViewController: BaseViewController {
-  @IBOutlet weak var sourceTitle: UILabel!
+  @IBOutlet weak var sourceTitleLabel: UILabel!
   @IBOutlet weak var backButton: UIButton!
+  @IBOutlet weak var cardsTableView: UITableView!
+  @IBOutlet weak var sourceCardsCountLabel: UILabel!
   
   var id: Int?
+  var source: Source?
   
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
   override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
     sourceRepository.get(id: id!) { source in
+      self.source = source
+      
       DispatchQueue.main.async {
-        self.sourceTitle.text = source.title
+        self.sourceTitleLabel.text = self.source!.title
+        self.sourceCardsCountLabel.text = String(format: "Cards: %d", self.source?.cards?.count ?? 0)
+        self.cardsTableView.reloadData()
       }
     }
+  }
+}
+
+extension SourceViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.source?.cards?.count ?? 0
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "card")
+    cell.textLabel?.text = source!.cards![indexPath.row].content
+    
+    return cell
   }
 }
