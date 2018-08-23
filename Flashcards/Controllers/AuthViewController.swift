e98cdef6782bb5a5c5d585d73d26bcdd9ff482ee
Sergei Novikov <novikov359@gmail.com>
2018-08-23 21:56:38 +0300

    Add DEBUG_TOKEN for DEBUG auth

--- Flashcards/Controllers/AuthViewController.swift
+++ Flashcards/Controllers/AuthViewController.swift
@@ -13,13 +13,19 @@ class AuthViewController: BaseViewController {
   @IBOutlet weak var passwordTextField: UITextField!
   @IBOutlet weak var authButton: UIButton!
   @IBOutlet weak var errorLabel: UILabel!
-  
+
   override func viewDidAppear(_ animated: Bool) {
     authButton.layer.cornerRadius = 5
     errorLabel.isHidden = true
   }
   
   @IBAction func AuthButton(_ sender: UIButton) {
+    #if DEBUG
+      self.token = ProcessInfo.processInfo.environment["DEBUG_TOKEN"]
+      self.performSegue(withIdentifier: "openSources", sender: nil)
+      return
+    #endif
+    
     tokenRepository.create(email: emailTextField.text!, password: passwordTextField.text!) { token, err in
       if err != nil {
         DispatchQueue.main.async {
